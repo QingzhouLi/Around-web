f5bf4215b61f5d3feed4a65e1cf8d257d8bac85d
QingzhouLi <39134900+QingzhouLi@users.noreply.github.com>
2019-03-23 18:37:30 -0400

    add Register.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Register} from "./Register"
+import {Login} from "./Login"
 
 export class Main extends React.Component{
     render(){
         return (
-            <div>Main</div>
+            <div>
+                <Register />
+            </div>
         );
     }
 }
+
