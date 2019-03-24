73dd0ad9e9a4674ecdf7d10db45a51f954c46be8
QingzhouLi <39134900+QingzhouLi@users.noreply.github.com>
2019-03-24 17:26:58 -0400

    fire api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-    Form, Icon, Input, Button
+    Form, Icon, Input, Button, message
 } from 'antd';
+import { API_ROOT } from "../constants";
 
 class NormalLoginForm extends React.Component {
     handleSubmit = (e) => {
@@ -9,37 +10,59 @@ class NormalLoginForm extends React.Component {
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                fetch(`${API_ROOT}/login`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        username: values.username,
+                        password: values.password
+                    })
+                })
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.text();
+                        }
+                        throw new Error(response.statusText);
+                    })
+                    .then((data) => {
+                        message.success("Login Successed! ");
+                        console.log(data)
+                    })
+                    .catch((err) =>{ 
+                        message.error("Login Failed! ");
+                        console.log(err)
+                    })
             }
-        });
-    }
-
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <Form.Item>
-                    {getFieldDecorator('userName', {
-                        rules: [{ required: true, message: 'Please input your username!' }],
-                    })(
-                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-            </Button>
-                    Or <a href="">register now!</a>
-                </Form.Item>
-            </Form>
+        }
         );
-    }
+}
+
+render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+        <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+                {getFieldDecorator('username', {
+                    rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+            </Form.Item>
+            <Form.Item>
+                {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+            </Button>
+                Or <a href="">register now!</a>
+            </Form.Item>
+        </Form>
+    );
+}
 }
 
 export const Login = Form.create({ name: 'LoginForm' })(NormalLoginForm);
