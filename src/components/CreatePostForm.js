5e2b2459bbc90cb14d3724f126a6a87016844d8f
QingzhouLi <39134900+QingzhouLi@users.noreply.github.com>
2019-04-01 22:46:26 -0400

    Create Post Form -UI

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import { Form, Input, Upload, Icon } from 'antd'
+    ;
+class NormalCreatPostForm extends React.Component {
+    normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    }
 
-export class CreatePostForm extends React.Component{
-    render(){
+    beforeUpload = () => false;
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+
+
         return (
-            <div>
-                CreatePostForm
-            </div>
+            <Form>
+                <Form.Item
+                    label="Message"
+                >
+                    {getFieldDecorator('message', {
+                        rules: [{
+                            required: true, message: "Please input your message!",
+                        }],
+                    })(
+                        <Input placeholder="Pleace input your message!" />
+                    )}
+                </Form.Item>
+
+                <Form.Item
+                    label="Image"
+                >
+                    <div className="dropbox">
+                        {getFieldDecorator('image', {
+                            rules: [{
+                                required: true, message: "Please select an image!",
+                            }],
+                            valuePropName: 'fileList',
+                            getValueFromEvent: this.normFile,
+                        })(
+                            <Upload.Dragger name="files" beforeUpload= {this.beforeUpload}>
+                                <p className="ant-upload-drag-icon">
+                                    <Icon type="inbox" />
+                                </p>
+                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                            </Upload.Dragger>
+                        )}
+                    </div>
+                </Form.Item>
+
+            </Form>
         )
     }
 }
+
+export const CreatePostForm = Form.create({ name: 'CreatePostForm' })(NormalCreatPostForm);
