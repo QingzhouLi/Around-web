7b7a22eb983e16e465aef0103e36c0d7b00baf10
QingzhouLi <39134900+QingzhouLi@users.noreply.github.com>
2019-04-08 19:39:56 -0400

    no message

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,42 +30,42 @@ class NormalLoginForm extends React.Component {
                         console.log(data)
                         this.props.handleLogin(data);
                     })
-                    .catch((err) =>{ 
+                    .catch((err) => {
                         message.error("Login Failed! ");
                         console.log(err)
                     })
             }
         }
         );
-}
+    }
 
-render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-        <Form onSubmit={this.handleSubmit} className="login-form">
-            <Form.Item>
-                {getFieldDecorator('username', {
-                    rules: [{ required: true, message: 'Please input your username!' }],
-                })(
-                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                )}
-            </Form.Item>
-            <Form.Item>
-                {getFieldDecorator('password', {
-                    rules: [{ required: true, message: 'Please input your Password!' }],
-                })(
-                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                )}
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
                 </Button>
-                Or <Link to="/register">register now!</Link>
-            </Form.Item>
-        </Form>
-    );
-}
+                    Or <Link to="/register">register now!</Link>
+                </Form.Item>
+            </Form>
+        );
+    }
 }
 
 export const Login = Form.create({ name: 'LoginForm' })(NormalLoginForm);
